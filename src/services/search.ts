b86c9c65c3a245a2420034f92ff7e6eeaabdd94c
firengxuan <568420804@qq.com>
2021-10-18 11:08:55 +0800

    fix: 修改背景显示问题

--- src/services/search.ts
+++ src/services/search.ts
@@ -1,6 +1,6 @@
 import request from "./request";
 export const querySuggestionRequest = async (keyword:string) => {
   return await request.get<null, Promise<any>>(
-    `https://www.baidu.com/sugrec?pre=1&p=3&ie=utf-8&json=1&prod=pc&from=pc_web&sugsid=34301,34333,34370,34145,34377,33848,34092,34111,26350,34319,22159,34390,34360&wd=${keyword}&bs=iterm%20%E6%8A%80%E5%B7%A7&csor=2&pwd=zf%20me123123&_=1628728146540`
+    `/baiduapi/sugrec?pre=1&p=3&ie=utf-8&json=1&prod=pc&from=pc_web&sugsid=34301,34333,34370,34145,34377,33848,34092,34111,26350,34319,22159,34390,34360&wd=${keyword}&bs=iterm%20%E6%8A%80%E5%B7%A7&csor=2&pwd=zf%20me123123&_=1628728146540`
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,20 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: "./",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  plugins: [reactRefresh()],
+    server: {
+        proxy: {
+            "/baiduapi": {
+                target: "https://www.baidu.com",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/baiduapi/, ""),
+            },
+        },
+    },
+    plugins: [reactRefresh()],
 });
